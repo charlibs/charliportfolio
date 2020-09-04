3a0940b6d2204172bc56080a50e68510e06f9e4e
Charli <hyejinsung7@gmail.com>
2020-09-04 13:03:14 -0700

    JavaScript Home Transparent

--- main.js
+++ main.js
@@ -32,7 +32,17 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+// Make home fade away sloway as scrolling down 
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
